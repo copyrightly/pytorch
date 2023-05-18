c58e2e3f579ea1b1505003ebdafeb149212f6d95
Stephen Jia <ssjia@meta.com>
2023-05-18 09:10:34 -0700

    [vulkan] Fix concat op in feature dimension (#101721)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/101721
    
    Fix a small bug in the `cat_feature` shader where an early exit path was not being taken correctly when an out of bounds channels was being accessed in the input tensor. The error was occuring because the channel limit was aligned to 4; passing in the original channels length fixed the issue.
    
    Test Plan:
    Referring to [Pytorch Vulkan Testing Procedures](https://www.internalfb.com/intern/wiki/Pytorch_Vulkan_Backend/Development/Vulkan_Testing_Procedures/):
    
    * Run operator unit tests on Mac and Android
    * Run model inference and correctness benchmarks
    
    Reviewed By: salilsdesai
    
    Differential Revision: D45962806
    
    fbshipit-source-id: 43a767ee6258a9810e7106ed18a1d29ee73c3b75

--- aten/src/ATen/native/vulkan/glsl/cat_feature.glsl
+++ aten/src/ATen/native/vulkan/glsl/cat_feature.glsl
@@ -26,10 +26,10 @@ layout(set = 0, binding = 2) uniform PRECISION restrict Block {
   uint batch_size;
   // input tensor's channel size
   uint ch_size;
-  // channel interval (total # of channels for all tensors)
-  uint ch_interval;
-  // # of channels for tensor 0 to i-1 at ith tensor
-  uint ch_size_allprior;
+  // total number of channels in the output tensor
+  uint ch_total;
+  // number of channels already appended
+  uint ch_current;
 }
 uBlock;
 
@@ -40,7 +40,6 @@ layout(local_size_x_id = 0, local_size_y_id = 1, local_size_z_id = 2) in;
 
 void main() {
   const ivec3 in_pos = ivec3(gl_GlobalInvocationID);
-  const uint max_src_index = uBlock.ch_size * uBlock.batch_size;
 
   if (any(greaterThanEqual(in_pos, uBlock.in_extents.xyz))) {
     return;
@@ -51,18 +50,17 @@ void main() {
   const vec4 in_tex = texelFetch(uInput, in_pos, 0);
 
   for (uint i = 0; i < 4; ++i) {
-    uint src_index = in_pos.z * 4 + i;
+    uint src_nc_idx = in_pos.z * 4 + i;
+    uint src_n_idx = src_nc_idx / uBlock.ch_size;
+    uint src_c_idx = src_nc_idx % uBlock.ch_size;
 
-    if (src_index >= max_src_index) {
+    if (src_c_idx >= uBlock.ch_size) {
       // out of range
       break;
     }
 
-    uint src_n_idx = src_index / uBlock.ch_size;
-    uint src_c_idx = src_index % uBlock.ch_size;
-
-    uint dst_nc_idx =
-        src_n_idx * uBlock.ch_interval + src_c_idx + uBlock.ch_size_allprior;
+    uint dst_c_idx = src_c_idx + uBlock.ch_current;
+    uint dst_nc_idx = src_n_idx * uBlock.ch_total + dst_c_idx;
 
     out_pos.z = int(dst_nc_idx / 4);
     uint j = (dst_nc_idx % 4);
